d2cfdb9c5249c48d0f0de929726e5f0c67b41504
PrashantSinghrgb <57823684+PrashantSinghrgb@users.noreply.github.com>
2023-09-26 01:46:10 +0530

    Add project files.

---
+++ ChallengeMasterVariableP_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLearning
+{
+    internal class ChallengeMasterVariableP_1
+    {
+        public static string name = "Prashant Singh";
+        private static int age = 20;
+        private static int speed = 15;
+        private static int health = 100;
+        private static int score = 25;
+        private static int ammoCount = 10;
+        private static bool checkIfAllTheKeysAreCollected = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine($"Hello {name}");
+            Console.WriteLine("My age : {0}",age);
+            Console.WriteLine(speed);
+            Console.WriteLine(health);
+            Console.WriteLine(score);
+            Console.WriteLine(ammoCount);
+            Console.WriteLine(checkIfAllTheKeysAreCollected);
+
+            Console.ReadLine();
+
+        }
+    }
+}
