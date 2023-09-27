38d8c508a109ed90f3146317876259efe0372f74
PrashantSinghrgb <57823684+PrashantSinghrgb@users.noreply.github.com>
2023-09-27 18:54:36 +0530

    Data types conversion

---
+++ Variables/DataTypesConversions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCSharpLearning.DataTypesConversions
+{
+    internal class DataTypesConversions
+    {
+        static void Main(string[] args)
+        {
+            int intValue = 10;
+            long longValue = intValue;
+            float floatValue = intValue;
+            double doubleValue = intValue;
+
+            Console.WriteLine(longValue);
+            Console.WriteLine(floatValue);
+            Console.WriteLine(doubleValue);
+
+
+            double doubleValuess = 10.5;
+            int intValuess = (int)doubleValuess;
+
+            Console.WriteLine(intValuess);
+
+            int intValues = 10;
+            string intStringValue = intValues.ToString();
+            long longValues = 1000152132132132156;
+            string longStringValue = longValues.ToString();
+            float floatValues = 10.5f;
+            string floatStringValue = floatValues.ToString();
+            double doubleValues = 3.14159;
+            string doubleStringValue = doubleValues.ToString();
+
+            Console.WriteLine(intStringValue);
+            Console.WriteLine(longStringValue);
+            Console.WriteLine(floatStringValue);
+            Console.WriteLine(doubleStringValue);
+
+            string fromStringToBool = "True";
+            bool convertStingToBool = bool.Parse(fromStringToBool);
+            string fromStringToInt = "42";
+            int convertStingToInt = int.Parse(fromStringToInt);
+            string fromStringToDouble = "1.234";
+            double convertStingToDouble = double.Parse(fromStringToDouble);
+
+            Console.WriteLine(convertStingToBool);
+            Console.WriteLine(convertStingToInt);
+            Console.WriteLine(convertStingToDouble);
+
+            Console.ReadLine();
+        }
+    }
+}
